e01e0d8a85cea7638030aeb5013c9fb20c7e06d9
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-11-10 22:58:25 +0100

    les potions

--- JamDruide/Assets/Scripts/ResourceScript.cs
+++ JamDruide/Assets/Scripts/ResourceScript.cs
@@ -1,8 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceScript : MonoBehaviour
 {
+    private CircleCollider2D collider2D;
     public CraftsList.Resources resourceType;
+    [SerializeField] private float resourceSpawnTime;
+
+    private void Start()
+    {
+        collider2D = GetComponent<CircleCollider2D>();
+    }
+
+    public void ResourceCollected()
+    {
+        collider2D.enabled = false;
+        StartCoroutine(NewResource());
+    }
+
+    private IEnumerator NewResource()
+    {
+        yield return new WaitForSeconds(resourceSpawnTime);
+        collider2D.enabled = true;
+    }
 }
