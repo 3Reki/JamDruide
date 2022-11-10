14d099387d57c6fe64b7a6ddbee44a6af28d1d0c
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-11-10 23:04:05 +0100

    oui monsieur

---
+++ JamDruide/Assets/CameraController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Vector3 cameraOffset;
+
+    private void LateUpdate()
+    {
+        
+    }
+}
