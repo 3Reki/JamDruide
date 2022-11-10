68255d8885f14fcb59fe5de8ad7f079d107931e7
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-11-10 22:41:01 +0100

    les recettes

---
+++ JamDruide/Assets/Scripts/CraftsList.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftsList : MonoBehaviour
+{
+    public List<Recipe> recipes;
+    public enum Resources
+    {
+        None, Mistletoe, Hydromel, Mushroom, Salt
+    }
+
+    [Serializable]
+    public class Recipe
+    {
+        public Resources[] ingredients = new Resources[2];
+        public GameObject output;
+        
+    }
+}
