060c0d4cf6cf8ad4946d334d754c9ef396fa9b45
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-11-11 17:30:01 +0100

    oui

--- JamDruide/Assets/Scripts/Potions/SplashAoEPotion.cs
+++ JamDruide/Assets/Scripts/Potions/SplashAoEPotion.cs
@@ -4,8 +4,10 @@ namespace Potions
 {
     public class SplashAoEPotion : MonoBehaviour
     {
+        [SerializeField] private GameObject particleSystem;
         private void OnTriggerStay2D(Collider2D col)
         {
+            Instantiate(particleSystem, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
--- JamDruide/Assets/Scripts/Potions/SplashDamagePotion.cs
+++ JamDruide/Assets/Scripts/Potions/SplashDamagePotion.cs
@@ -4,8 +4,10 @@ namespace Potions
 {
     public class SplashDamagePotion : MonoBehaviour
     {
+        [SerializeField] private GameObject particleSystem;
         private void OnTriggerEnter2D(Collider2D col)
         {
+            Instantiate(particleSystem, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
