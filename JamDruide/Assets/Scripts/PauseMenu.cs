04e27e6fcb2f87f1c95916064ba69148e3db3c15
LeBud <c.huyghe@rubika-edu.com>
2022-11-11 12:51:56 +0100

    PauseMenu

--- JamDruide/Assets/Scripts/PauseMenu.cs
+++ JamDruide/Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PauseMenu : MonoBehaviour
 {
 
     public static bool gameIsPause;
+
+    [SerializeField] GameObject gameHUD;
+    [SerializeField] GameObject pauseHUD;
 
     private void Update()
     {
@@ -30,6 +34,9 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = true;
         Time.timeScale = 0f;
+        OnPause.Invoke();
+        pauseHUD.SetActive(true);
+        gameHUD.SetActive(false);
     }
 
     void Resume()
@@ -37,6 +44,11 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Time.timeScale = 1f;
+        OnResume.Invoke();
+        pauseHUD.SetActive(false);
+        gameHUD.SetActive(true);
     }
 
+    public static UnityEvent OnPause = new UnityEvent();
+    public static UnityEvent OnResume = new UnityEvent();
 }
