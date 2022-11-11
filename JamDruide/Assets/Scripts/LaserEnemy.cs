f79b1c30f9002e027f0107aff86101cd66d717ea
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-11-11 11:58:57 +0100

    laser qui marche

--- JamDruide/Assets/Scripts/LaserEnemy.cs
+++ JamDruide/Assets/Scripts/LaserEnemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Player;
+using UnityEditor;
 using UnityEngine;
 
 public class LaserEnemy : MonoBehaviour
@@ -9,30 +10,57 @@ public class LaserEnemy : MonoBehaviour
     [SerializeField] private ParticleSystem laserStart;
     [SerializeField] private ParticleSystem laserEnd;
     private Vector2 laserDirection;
-    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private LineRenderer activeLineRenderer, inactiveLineRenderer;
+    [SerializeField] private float laserDuration;
+    [SerializeField] private float laserCastTime;
+    private float startTime;
+    private bool laserActive;
 
     private void Start()
     {
-        lineRenderer.SetPosition(0, transform.position);
+        activeLineRenderer.enabled = false;
+        activeLineRenderer.SetPosition(0, transform.position);
+        inactiveLineRenderer.SetPosition(0, transform.position);
         laserStart.Play();
         laserEnd.Play();
+        startTime = Time.time;
     }
 
     public void Update()
     {
         LaserCreation();
+        if (!laserActive && Time.time >= startTime + laserCastTime)
+        {
+            laserActive = true;
+            activeLineRenderer.enabled = true;
+            startTime = Time.time;
+        }
+
+        if (laserActive && Time.time >= startTime + laserDuration)
+        {
+            laserActive = false;
+            startTime = Time.time;
+            activeLineRenderer.enabled = false;
+        }
     }
 
     private void LaserCreation()
     {
         laserDirection = PlayerActions.Instance.transform.position- transform.position;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, laserDirection);
-        lineRenderer.SetPosition(1, hit.point);
-        laserEnd.transform.position = hit.point;
-        float angle = Vector2.Angle(hit.point, Vector2.right);
-        laserEnd.transform.LookAt(transform.position);
+        inactiveLineRenderer.SetPosition(1, hit.point);
+        if (laserActive)
+        {
+            activeLineRenderer.SetPosition(1, hit.point);
+            laserEnd.transform.position = hit.point;
+            float angle = Vector2.Angle(hit.point, Vector2.right);
+            laserEnd.transform.LookAt(transform.position);
+            if (hit.transform.CompareTag("Player"))
+            {
+                StartCoroutine(PlayerActions.Instance.Death());
+            }
+        }
         
-        Debug.Log(hit.transform.name);
     }
 
     private void OnDrawGizmos()
