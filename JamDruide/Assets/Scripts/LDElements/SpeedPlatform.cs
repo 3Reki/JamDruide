369d608dd73ceccc740224bc4e5bd0a81401fef7
OGER <cocoog77@gmail.com>
2022-11-11 22:16:34 +0100

    splash speed

---
+++ JamDruide/Assets/Scripts/LDElements/SpeedPlatform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace LDElements
+{
+    public class SpeedPlatform : MonoBehaviour
+    {
+        public bool IsActive;
+
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float platformSpeed;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Speed"))
+            {
+                IsActive = true;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (!IsActive)
+            {
+                return;
+            }
+            if (col.gameObject.CompareTag("Player"))
+            {
+                if (col.GetContact(0).normal == Vector2.right)
+                {
+                    IsActive = false;
+                    rb.velocity = Vector2.right * platformSpeed;
+                    return;
+                }
+                
+                if (col.GetContact(0).normal == Vector2.left)
+                {
+                    IsActive = false;
+                    rb.velocity = Vector2.left * platformSpeed;
+                }
+            }
+        }
+    }
+}
