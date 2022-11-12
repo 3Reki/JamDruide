a518d77d2e26545bca6467a803f088bc7c53e7c2
OGER <cocoog77@gmail.com>
2022-11-12 06:11:15 +0100

    Camera down

--- JamDruide/Assets/CameraController.cs
+++ JamDruide/Assets/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
         public Transform target;
         public float smoothTime = 0.3F;
         private Vector3 velocity = Vector3.zero;
+        public Vector3 offset = new Vector3(0, 2, -10);
     private void OnEnable()
     {
         PauseMenu.OnPause.AddListener(() => enabled = false);
@@ -24,7 +25,12 @@ public class CameraController : MonoBehaviour
     void Update()
         {
             // Define a target position above and behind the target transform
-            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 2, -10));
+            Vector3 targetPosition = target.TransformPoint(offset);
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                targetPosition += new Vector3(0, -5);
+            }
      
             // Smoothly move the camera towards that target position
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
