680c1b1facd1042422e76a6414345fccbaeb7c25
Rayki <cocoog77@gmail.com>
2022-11-13 21:54:59 +0100

    Gamepad menu nav

---
+++ JamDruide/Assets/Scripts/AutoFocus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AutoFocus : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(target);
+    }
+}
